f167f2ff8c1633eea918db2f5bedbb41bde808f0
Frederik Lührs <luehrs.fred@gmail.com>
2021-05-04 15:04:10 +0200

    remove room_ensurer and purger

--- tests/file_templates.py
+++
@@ -1,1 +0,0 @@
-USER_PRESENCE_TEMPLATE = '{"last_update": ${last_update}, "network_to_users": ${network_to_users}}'
--- tests/utils.py
+++ tests/utils.py
@@ -34,25 +34,12 @@ class GMatrixHttpApiTest(GMatrixHttpApi):
 
     # this is a hack because _send is only called upon fetching room members via admin api
     def _send(
-        self, method, path, content=None, query_params=None, headers=None, api_path="",
+        self,
+        method,
+        path,
+        content=None,
+        query_params=None,
+        headers=None,
+        api_path="",
     ):
         return {"members": []}
-
-
-def create_user_activity_dict(size: int, lower_bound: int, upper_bound: int) -> Dict[str, int]:
-    """
-
-    :param size: number of users
-    :param lower_bound: lower bound of how many seconds ago the user was seen online
-    :param upper_bound: higher bound of how many seconds ago the user was seen online
-    :return: dictionary of users to last seen online age in seconds
-    """
-    users = {}
-    current_time = int(time.time())
-    for _ in range(size):
-        user_id = create_new_users_for_address()[0].user_id
-        last_active_ago = randint(lower_bound, upper_bound)
-        last_active_seen = current_time - last_active_ago
-        users[user_id] = last_active_seen
-
-    return users
